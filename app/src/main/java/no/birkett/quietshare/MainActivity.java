2fc42661ba732beb6cf02427cd46a5853616d8f3
Alex Birkett <alex@agens.no>
2016-10-22 18:04:36 +0200

    Request audio permission

--- app/src/main/java/no/birkett/quietshare/MainActivity.java
+++ app/src/main/java/no/birkett/quietshare/MainActivity.java
@@ -1,9 +1,15 @@
 package no.birkett.quietshare;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
@@ -15,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
     private FrameReceiver receiver;
     private FrameTransmitter transmitter;
     private TextView receivedContent;
+    
+    private static final int MY_PERMISSIONS_REQUEST_RECORD_AUDIO = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,37 +37,69 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.receive).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                receive();
+                handleReceiveClick();
             }
         });
         receivedContent = (TextView) findViewById(R.id.received_content);
-        setup();
+        setupTransmitter();
     }
 
-    private void setup() {
-        FrameReceiverConfig receiverConfig;
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_RECORD_AUDIO: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    setupReceiver();
+                    receive();
+                } else {
+                    showMissingAudioPermissionToast();
+                }
+            }
+        }
+    }
+
+    private void setupTransmitter() {
         FrameTransmitterConfig transmitterConfig;
         try {
             transmitterConfig = new FrameTransmitterConfig(
                     this,
                     "audible-7k-channel-0");
-            receiverConfig = new FrameReceiverConfig(
-                    this,
-                    "audible-7k-channel-0");
+
+            transmitter = new FrameTransmitter(transmitterConfig);
         } catch (IOException e) {
             throw new RuntimeException(e);
-        }
-
-        try {
-            receiver = new FrameReceiver(receiverConfig);
-            transmitter = new FrameTransmitter(transmitterConfig);
         } catch (ModemException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public void receive() {
-        // set receiver to block for at most 20 seconds
+    private void setupReceiver() {
+        try {
+            FrameReceiverConfig receiverConfig = new FrameReceiverConfig(this, "audible-7k-channel-0");
+            receiver = new FrameReceiver(receiverConfig);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ModemException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void handleReceiveClick() {
+        if (receiver == null) {
+            if (hasRecordAudioPersmission()) {
+                setupReceiver();
+            } else {
+                requestPermission();
+            }
+        } else {
+            receive();
+        }
+    }
+
+    private void receive() {
         receiver.setBlocking(5, 0);
 
         byte[] buf = new byte[1024];
@@ -71,13 +111,31 @@ public class MainActivity extends AppCompatActivity {
             // read timed out
         }
     }
-
-    public void send() {
+    private void send() {
         String payload = "Hello, World!";
         try {
             transmitter.send(payload.getBytes());
         } catch (IOException e) {
             // our message might be too long or the transmit queue full
         }
+    }
+
+    boolean hasRecordAudioPersmission() {
+        return ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.RECORD_AUDIO},
+                MY_PERMISSIONS_REQUEST_RECORD_AUDIO);
+    }
+
+
+    private void showMissingAudioPermissionToast() {
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, R.string.missing_audio_permission, duration);
+        toast.show();
     }
 }
